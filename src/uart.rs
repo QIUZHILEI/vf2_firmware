7cd5ad0d0baf7873cf4bf2b48807849ae998c430
QIUZHILEI <zhilei.oerv@isrc.iscas.ac.cn>
2024-12-11 10:59:06 +0800

    init code
    Signed-off-by: QIUZHILEI <zhilei.oerv@isrc.iscas.ac.cn>

---
+++ src/uart.rs
@@ -0,0 +1,42 @@
+use lego_device::{CharDevice, Device};
+use uart_8250::Uart;
+
+pub const UART_BASE: usize = 0x10000000;
+
+pub static mut UART: UartWrapper = UartWrapper::new(UART_BASE);
+pub struct UartWrapper {
+    uart: Uart,
+}
+
+impl UartWrapper {
+    const fn new(base_addr: usize) -> Self {
+        Self {
+            uart: Uart::new(base_addr, 13),
+        }
+    }
+}
+
+impl core::fmt::Write for UartWrapper {
+    fn write_str(&mut self, s: &str) -> core::fmt::Result {
+        s.as_bytes().into_iter().for_each(|byte| {
+            self.uart.put_char(*byte).unwrap();
+        });
+        Ok(())
+    }
+}
+#[macro_export]
+macro_rules! println {
+    () => {{
+        use core::fmt::Write;
+        writeln!(unsafe{(&raw mut $crate::UART).as_mut().unwrap()}).unwrap();
+    }};
+    ($($arg:tt)*) => {{
+        use core::fmt::Write;
+        writeln!(unsafe{(&raw mut $crate::UART).as_mut().unwrap()},$($arg)*).unwrap();
+    }};
+}
+
+pub fn init_uart() {
+    let uart_ref = unsafe { (&raw mut UART).as_mut().unwrap() };
+    uart_ref.uart.init().unwrap();
+}
